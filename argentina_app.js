1fcb15fe7a26fb772b9e7f919d94b38cf2880f46
unknown <ddoucett@ucsd.edu>
2021-04-20 08:49:12 -0700

    new commit

--- argentina_app.js
+++ argentina_app.js
@@ -10,119 +10,11 @@ function selectData(selectedCountry){
   // Check if value is selected in dropdown
   console.log(selectedCountry);
 
-var margin = {top: 100, right: 0, bottom: 0, left: 0},
-  width = 460 - margin.left - margin.right,
-  height = 460 - margin.top - margin.bottom,
-  innerRadius = 90,
-  outerRadius = Math.min(width, height) / 2;   // the outerRadius goes from the middle of the SVG area to the border
-
-// append the svg object
-var svg = d3.select("#my_dataviz")
-.append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-.append("g")
-  .attr("transform", "translate(" + (width / 2 + margin.left) + "," + (height / 2 + margin.top) + ")");
-
-
 // Read the json file for the data
-  d3.csv("wine.csv").then((data) => {
+  d3.csv("Australia.csv").then((data) => {
   
   wineData=data
   // console.log(data);
-
-  // WORD CLOUD
-
-  // List of words
-var wineTypes = [];
-
-for(i=0; i<data.length; i++){
-  if(wineTypes.indexOf(data[i].variety) === -1){
-    wineTypes.push(data[i].variety);
-  }
-}
-// console.log(wineTypes);
-
-// // Scales
-// var x = d3.scaleBand()
-// .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
-// .align(0)                  // This does nothing
-// .domain(data.map(function(d) { return d.points; })); // The domain of the X axis is the list of states.
-// var y = d3.scaleRadial()
-// .range([innerRadius, outerRadius])   // Domain will be define later.
-// .domain([0, 14000]); // Domain of Y is from 0 to the max seen in the data
-
-// // Add the bars
-// svg.append("g")
-// .selectAll("path")
-// .data(data)
-// .enter()
-// .append("path")
-// .attr("fill", "#69b3a2")
-// .attr("d", d3.arc()     // imagine your doing a part of a donut plot
-//     .innerRadius(innerRadius)
-//     .outerRadius(function(d) { return y(d['Value']); })
-//     .startAngle(function(d) { return x(d.country); })
-//     .endAngle(function(d) { return x(d.country) + x.bandwidth(); })
-//     .padAngle(0.01)
-//     .padRadius(innerRadius))
-
-// // Add the labels
-// svg.append("g")
-// .selectAll("g")
-// .data(data)
-// .enter()
-// .append("g")
-//   .attr("text-anchor", function(d) { return (x(d.country) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
-//   .attr("transform", function(d) { return "rotate(" + ((x(d.country) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (y(d['Value'])+10) + ",0)"; })
-// .append("text")
-//   .text(function(d){return(d.country)})
-//   .attr("transform", function(d) { return (x(d.country) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
-//   .style("font-size", "11px")
-//   .attr("alignment-baseline", "middle")
-
-
-
-// // set the dimensions and margins of the graph
-// var margin = {top: 10, right: 10, bottom: 10, left: 10},
-//     width = 450 - margin.left - margin.right,
-//     height = 450 - margin.top - margin.bottom;
-
-// // append the svg object to the body of the page
-// var svg = d3.select("#word-cloud").append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform",
-//           "translate(" + margin.left + "," + margin.top + ")");
-
-// // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
-// var layout = d3.layout.cloud()
-//   .size([width, height])
-//   .words(wineTypes.map(function(d) { return {text: d}; }))
-//   .padding(10)
-//   .fontSize(60)
-//   .on("end", draw);
-// layout.start();
-
-// // This function takes the output of 'layout' above and draw the words
-// // Better not to touch it. To change parameters, play with the 'layout' variable above
-// function draw(words) {
-//   svg
-//     .append("g")
-//       .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
-//       .selectAll("text")
-//         .data(words)
-//       .enter().append("text")
-//         .style("font-size", function(d) { return d.size + "px"; })
-//         .attr("text-anchor", "middle")
-//         .attr("transform", function(d) {
-//           return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-//         })
-//         .text(function(d) { return d.text; });
-// }
-
-// CIRCLES CHART
   
   wineData.forEach(function(data) {
           data.price = +data.price;
@@ -130,110 +22,69 @@ for(i=0; i<data.length; i++){
         });
 
   
-  // var xLinearScale = d3.scaleLinear()
-  //       .domain([0, d3.max(wineData, d => d.price)])
-  //       .range([0, width]);
-        
-  // var yLinearScale = d3.scaleLinear()
-  //       .domain([75, d3.max(wineData, d => d.points)])
-  //       .range([height, 0]);
-        
-  // var bottomAxis = d3.axisBottom(xLinearScale);
-  // var leftAxis = d3.axisLeft(yLinearScale);
-
-  // chartGroup.append("g")
-  //     .attr("transform", `translate(0, ${height})`)
-  //     .call(bottomAxis);
-
-  // chartGroup.append("g")
-  //     .call(leftAxis);
-  
-  // var circlesGroup = chartGroup.selectAll("circle")
-  //     .data(wineData)
-  //     .enter()
-  //     .append("circle")
-  //     .attr("cx", d => xLinearScale(d.price))
-  //     .attr("cy", d => yLinearScale(d.points))
-  //     .attr("r", "4")
-  //     .attr("fill", "red")
-  //     .attr("opacity", ".5");
-  
-      
-  // var toolTip = d3.tip()
-  //     .attr("class", "tooltip")
-  //     .offset([80, -60])
-  //     .html(function(d) {
-  //       return (`${d.winery}<br>Winery: ${d.winery}<br>Points: ${d.points}`);
-  //     }); 
-
-  // chartGroup.call(toolTip);
-
-  // circlesGroup.on("click", function(data) {
-  //         toolTip.show(data, this);
-  //       })
-  //         // onmouseout event
-  //         .on("mouseout", function(data, index) {
-  //           toolTip.hide(data);
-  //         });
-    
-  //       chartGroup.append("text")
-  //         .attr("transform", "rotate(-90)")
-  //         .attr("y", 0 - margin.left + 40)
-  //         .attr("x", 0 - (height / 2))
-  //         .attr("dy", "1em")
-  //         .attr("class", "axisText")
-  //         .text("Price");
-    
-  //       chartGroup.append("text")
-  //         .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-  //         .attr("class", "axisText")
-  //         .text("Points");
-  //     }).catch(function(error) {
-  //       console.log(error);
-  //     });
-
-  var uniqueCountries = [];
+  // var uniqueCountries = [];
   var uniqueWineries = [];
 
-  for(i=0; i<data.length; i++){
-    if(uniqueCountries.indexOf(data[i].country) === -1){
-      uniqueCountries.push(data[i].country);
-    }
-  }
-
   // for(i=0; i<data.length; i++){
-  //   if(uniqueWineries.indexOf(data[i].winery) === -1){
-  //     uniqueWineries.push(data[i].winery);
+  //   if(uniqueCountries.indexOf(data[i].country) === -1){
+  //     uniqueCountries.push(data[i].country);
   //   }
   // }
 
-  // console.log(uniqueCountries);
-  
-  
-  // Select the uniqueCountries array and for each item append the item ID and adds ID to dropdown
-  uniqueCountries.forEach(country =>
-       {
-  //       // console.log(item.id);
-       d3.select("#selDataset").append('option').attr('value', country).text(country);
-       });
+  for(i=0; i<data.length; i++){
+    if(uniqueWineries.indexOf(data[i].winery) === -1){
+      uniqueWineries.push(data[i].winery);
+    }
+  }
 
-  // Selected value is passed
-  d3.select("#selDataset").node().value = selectedCountry;
+  // console.log(uniqueCountries);
+  console.log(uniqueWineries);
   
-  // Filter data for selected country from dropdown
-  var countryData = data.filter(winery=> (winery.country == selectedCountry));
+  // // Select the uniqueCountries array and for each item append the item ID and adds ID to dropdown
+  // uniqueCountries.forEach(country =>
+  //      {
+  // //       // console.log(item.id);
+  //      d3.select("#selDataset").append('option').attr('value', country).text(country);
+  //      });
+
+  // // Selected value is passed
+  // d3.select("#selDataset").node().value = selectedCountry;
+  
+    // Select the uniqueCountries array and for each item append the item ID and adds ID to dropdown
+    uniqueWineries.forEach(winery =>
+      {
+ //       // console.log(item.id);
+      d3.select("#selDataset").append('option').attr('value', winery).text(winery);
+      });
+
+
+//  // Selected value is passed
+//  d3.select("#selDataset").node().value = selectedCountry;
+//   // Filter data for selected country from dropdown
+//   var countryData = data.filter(winery=> (winery.country == selectedCountry));
     
+  d3.select("#selDataset").node().value = selectedCountry;
+  // Filter data for selected country from dropdown
+  var countryData = data.filter(winery=> (winery.winery == selectedCountry));
 //   // Check the data loaded for the selected country
   // console.log(countryData);
   
   // Update the panel display to have the selected country
-  var panelDisplay = d3.select("#sample-data");
-  panelDisplay.html("");
-  Object.entries(countryData[0]).forEach(winery=> 
-     {
-        console.log(winery);
-        panelDisplay.append("p").text(`${winery[0]}: ${winery[1]}`)
-     });
+  // var panelDisplay = d3.select("#sample-data");
+  // panelDisplay.html("");
+  // Object.entries(countryData[0]).forEach(winery=> 
+  //    {
+  //       console.log(winery);
+  //       panelDisplay.append("p").text(`${winery[0]}: ${winery[1]}`)
+  //    });
+
+     var panelDisplay = d3.select("#sample-data");
+     panelDisplay.html("");
+     Object.entries(countryData[0]).forEach(winery=> 
+        {
+           console.log(winery);
+           panelDisplay.append("p").text(`${winery[0]}: ${winery[1]}`)
+        });
 
 //   // BAR CHART
 
--- australia_app.js
+++ australia_app.js
@@ -8,17 +8,32 @@ var margin = {
   left: 100
 };
 
+var margin2 = {top: 100, right: 0, bottom: 0, left: 0},
+  width = 460 - margin.left - margin.right,
+  height = 460 - margin.top - margin.bottom,
+  innerRadius = 90,
+  outerRadius = Math.min(width, height) / 2;   // the outerRadius goes from the middle of the SVG area to the border
+
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".chart")
+var svg1 = d3.select("#circle")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
-var chartGroup = svg.append("g")
+// append the svg object
+var svg2 = d3.select("#sunburst")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform", "translate(" + (width / 2 + margin.left) + "," + (height / 2 + margin.top) + ")");
+
+var chartGroup1 = svg1.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
 
 // Import Data
 d3.csv("Australia.csv").then(function(AustraliaWineData) {
@@ -47,16 +62,16 @@ d3.csv("Australia.csv").then(function(AustraliaWineData) {
 
     // Step 4: Append Axes to the chart
     // ==============================
-    chartGroup.append("g")
+    chartGroup1.append("g")
       .attr("transform", `translate(0, ${height})`)
       .call(bottomAxis);
 
-    chartGroup.append("g")
+    chartGroup1.append("g")
       .call(leftAxis);
 
     // Step 5: Create Circles
     // ==============================
-    var circlesGroup = chartGroup.selectAll("circle")
+    var circlesGroup = chartGroup1.selectAll("circle")
     .data(AustraliaWineData)
     .enter()
     .append("circle")
@@ -77,7 +92,7 @@ d3.csv("Australia.csv").then(function(AustraliaWineData) {
 
     // Step 7: Create tooltip in the chart
     // ==============================
-    chartGroup.call(toolTip);
+    chartGroup1.call(toolTip);
 
     // Step 8: Create event listeners to display and hide the tooltip
     // ==============================
@@ -90,7 +105,7 @@ d3.csv("Australia.csv").then(function(AustraliaWineData) {
       });
 
     // Create axes labels
-    chartGroup.append("text")
+    chartGroup1.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left + 40)
       .attr("x", 0 - (height / 2))
@@ -98,10 +113,135 @@ d3.csv("Australia.csv").then(function(AustraliaWineData) {
       .attr("class", "axisText")
       .text("Price");
 
-    chartGroup.append("text")
+    chartGroup1.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
       .text("Points");
   }).catch(function(error) {
     console.log(error);
   });
+
+    // List of words
+    var wineTypes = [];
+
+    for(i=0; i<data.length; i++){
+      if(wineTypes.indexOf(data[i].variety) === -1){
+        wineTypes.push(data[i].variety);
+      }
+    }
+    // console.log(wineTypes);
+
+  // Sunburst Chart
+
+  d3.csv("wine.csv").then((data) => {
+  
+    wineData=data
+    // console.log(data);
+  
+    // WORD CLOUD
+  
+    // List of words
+  var wineTypes = [];
+  
+  for(i=0; i<data.length; i++){
+    if(wineTypes.indexOf(data[i].variety) === -1){
+      wineTypes.push(data[i].variety);
+    }
+  }
+  // console.log(wineTypes);
+  
+  // // Scales
+  var x = d3.scaleBand()
+  .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
+  .align(0)                  // This does nothing
+  .domain(data.map(function(d) { return d.points; })); // The domain of the X axis is the list of states.
+  var y = d3.scaleRadial()
+  .range([innerRadius, outerRadius])   // Domain will be define later.
+  .domain([0, 14000]); // Domain of Y is from 0 to the max seen in the data
+  
+  // // Add the bars
+  svg2.append("g")
+  .selectAll("path")
+  .data(data)
+  .enter()
+  .append("path")
+  .attr("fill", "#69b3a2")
+  .attr("d", d3.arc()     // imagine your doing a part of a donut plot
+      .innerRadius(innerRadius)
+      .outerRadius(function(d) { return y(d['Value']); })
+      .startAngle(function(d) { return x(d.country); })
+      .endAngle(function(d) { return x(d.country) + x.bandwidth(); })
+      .padAngle(0.01)
+      .padRadius(innerRadius))
+  
+  // // Add the labels
+  svg2.append("g")
+  .selectAll("g")
+  .data(data)
+  .enter()
+  .append("g")
+    .attr("text-anchor", function(d) { return (x(d.country) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
+    .attr("transform", function(d) { return "rotate(" + ((x(d.country) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (y(d['Value'])+10) + ",0)"; })
+  .append("text")
+    .text(function(d){return(d.country)})
+    .attr("transform", function(d) { return (x(d.country) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
+    .style("font-size", "11px")
+    .attr("alignment-baseline", "middle")
+  
+  
+  
+  // // set the dimensions and margins of the graph
+  // var margin = {top: 10, right: 10, bottom: 10, left: 10},
+  //     width = 450 - margin.left - margin.right,
+  //     height = 450 - margin.top - margin.bottom;
+  
+  // // append the svg object to the body of the page
+  // var svg = d3.select("#word-cloud").append("svg")
+  //     .attr("width", width + margin.left + margin.right)
+  //     .attr("height", height + margin.top + margin.bottom)
+  //   .append("g")
+  //     .attr("transform",
+  //           "translate(" + margin.left + "," + margin.top + ")");
+  
+  // // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
+  // var layout = d3.layout.cloud()
+  //   .size([width, height])
+  //   .words(wineTypes.map(function(d) { return {text: d}; }))
+  //   .padding(10)
+  //   .fontSize(60)
+  //   .on("end", draw);
+  // layout.start();
+  
+  // // This function takes the output of 'layout' above and draw the words
+  // // Better not to touch it. To change parameters, play with the 'layout' variable above
+  // function draw(words) {
+  //   svg
+  //     .append("g")
+  //       .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+  //       .selectAll("text")
+  //         .data(words)
+  //       .enter().append("text")
+  //         .style("font-size", function(d) { return d.size + "px"; })
+  //         .attr("text-anchor", "middle")
+  //         .attr("transform", function(d) {
+  //           return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+  //         })
+  //         .text(function(d) { return d.text; });
+  // }
+  
+    var uniqueCountries = [];
+    var uniqueWineries = [];
+  
+    for(i=0; i<data.length; i++){
+      if(uniqueCountries.indexOf(data[i].country) === -1){
+        uniqueCountries.push(data[i].country);
+      }
+    }
+  
+    // for(i=0; i<data.length; i++){
+    //   if(uniqueWineries.indexOf(data[i].winery) === -1){
+    //     uniqueWineries.push(data[i].winery);
+    //   }
+    // }
+
+  })
